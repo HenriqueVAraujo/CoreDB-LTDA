e580e836621815418fe08e654acba68e9c279a27
Henrique Vasconcelos <henriquevasconcelosaraujo@gmail.com>
2026-02-12 17:20:55 -0300

    fix: adicionando base no vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   root: "client",
+  base: "/", // 🔥 IMPORTANTE
   plugins: [react()],
   build: {
     outDir: "../dist",
     emptyOutDir: true
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client/src")
-    }
   }
-})
+});
