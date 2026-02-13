57658973dcd4ccf5a2132c02778f150661287e5d
Henrique Vasconcelos <henriquevasconcelosaraujo@gmail.com>
2026-02-13 14:08:49 -0300

    inclusão das paginas de politica e termos de uso

--- src/pages/termos-de-uso.tsx
+++ src/pages/termos-de-uso.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FileText, Gavel, ShieldAlert, Copyright, ChevronLeft, MessageSquare, ExternalLink } from 'lucide-react';
-import Link from 'next/link';
+import { Link } from 'wouter';
 
 export default function TermsOfUse() {
   return (
