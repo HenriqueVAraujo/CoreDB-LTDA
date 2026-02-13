29e62150c4d92786b1ad3242d96bde3f04be9659
Henrique Vasconcelos <henriquevasconcelosaraujo@gmail.com>
2026-02-13 09:59:04 -0300

    fix: ajuste de diretório de build e configurações para vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; // <-- ADICIONE ISSO
+import tailwindcss from "@tailwindcss/vite"; 
 import path from "path";
 
 export default defineConfig({
   base: "/",
   plugins: [
     react(),
-    tailwindcss(), // <-- ADICIONE ISSO AQUI
+    tailwindcss(), 
   ],
   resolve: {
     alias: {
